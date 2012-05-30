41badc86e08a200268e2e192bd304f571bf33fd8
xiao liang <feexon@qq.com>
2012-05-30 22:55:08 +0800

    add failing test

---
+++ money/src/test/java/com/feexon/money/DollarTest.java
@@ -0,0 +1,16 @@
+package com.feexon.money;
+
+import org.junit.Test;
+
+/**
+ * @author Administrator
+ * @version 1.0 12-5-30,下午10:53
+ */
+public class DollarTest {
+    @Test
+    public void testMultiplication() throws Exception {
+        Dollar fiveBucks = new Dollar(5);
+        fiveBucks.times(2);
+        assertEquals(10, fiveBucks.amount);
+    }
+}
